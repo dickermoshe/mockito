8c5a634425475eee80fb4dced1762a2eb6989d7c
Moshe Dicker <dickermoshe@gmail.com>
2024-10-29 13:05:39 -0400

    parse the contents of a typedef

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -316,6 +316,7 @@ class _TypeVisitor extends RecursiveElementVisitor<void> {
 
   @override
   void visitTypeAliasElement(TypeAliasElement element) {
+    _addType(element.aliasedType);
     _elements.add(element);
     super.visitTypeAliasElement(element);
   }
